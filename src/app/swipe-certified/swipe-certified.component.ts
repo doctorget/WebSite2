77bcfb7468a7268dba29ec0876f6f833e11ec3b8
doctorget <samuelsoares177778@gmail.com>
2020-05-25 10:27:38 -0300

    swipe certified ok

---
+++ src/app/swipe-certified/swipe-certified.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'swipe-certified',
+  templateUrl: './swipe-certified.component.html',
+  styleUrls: ['./swipe-certified.component.scss']
+})
+export class SwipeCertifiedComponent implements OnInit {
+
+  @Input('data') data: any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
