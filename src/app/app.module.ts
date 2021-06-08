914f7d67891a73d09f969794753c192b32e14487
samuelsoaress <samuelsoares177778@gmail.com>
2021-06-08 09:52:16 -0300

    modificação nas urls e deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,9 @@ import { SkillsComponent } from './shared/components/skills/skills.component';
 import { GalleryComponent } from './shared/components/gallery/gallery.component';
 import { SwipeCertifiedComponent } from './swipe-certified/swipe-certified.component';
 import { NgxTypedJsComponent } from './ngx-typed-js/ngx-typed-js.component';
-import { SwiperModule, SWIPER_CONFIG,SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
--- src/app/swipe-certified/swipe-certified.component.ts
+++ src/app/swipe-certified/swipe-certified.component.ts
@@ -1,22 +1,72 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, AfterViewInit, Input, ElementRef, OnInit } from '@angular/core';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { Swiper, SwiperOptions } from 'swiper';
+
 
 @Component({
   selector: 'swipe-certified',
   templateUrl: './swipe-certified.component.html',
   styleUrls: ['./swipe-certified.component.scss']
 })
+
 export class SwipeCertifiedComponent implements OnInit {
 
+  constructor(
+    private elementRef: ElementRef
+  ) { }
   @Input('data') data: any;
-  configCustom:SwiperConfigInterface
-  constructor() { }
+  configCustom: SwiperConfigInterface = {
+    direction: 'horizontal',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    centeredSlides: true,
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    touchRatio: 0.2,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    slideToClickedSlide: true,
+  }
+
+  public swiper2 = new Swiper('.swiper-container .product-photos-side', this.configCustom);
+  config: SwiperConfigInterface =
+    {
+      direction: 'horizontal',
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      centeredSlides: true,
+      spaceBetween: 30,
+      slidesPerView: 'auto',
+      touchRatio: 0.2,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      slideToClickedSlide: true,
+      parallax: true,
+      controller:{
+        control: this.swiper2
+      }
+    }
+    public swiper = new Swiper('.product-photo-main .swiper-container', this.config)
+  
 
   ngOnInit(): void {
-    this.configCustom ={
-      direction:'horizontal',
-      navigation:false
-    }
+
+
+
+    
+
+  }
+  ngAfterViewInit(): void {
+    // this.swiperView.params.control = this.swiper;
+    // this.swiper.params.control = this.swiperView;
   }
 
 }
